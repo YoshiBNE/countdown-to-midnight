5af973e854cb48c876b35c30b413afded4d63fcf
YoshiBNE <yoshiyasu_express@me.com>
2022-06-30 23:04:42 +1000

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(intArg)
+    while(intArg >= 1)
+        puts "#{intArg} SECOND(S)!"
+        intArg -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(intArg)
+    until(intArg == 0)
+        sleep 1
+        puts "#{intArg} SECOND(S)!"
+        intArg -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
